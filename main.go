fbd9790d518e16f68a363d319d43b441efb8c3eb
Bjarki Madsen <bjarkimadsen@gmail.com>
2022-12-13 11:36:51 +0100

    let's try

--- main.go
+++ main.go
@@ -116,7 +116,30 @@ func move(state GameState) BattlesnakeMoveResponse {
 	}
 
 	// TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-	// opponents := state.Board.Snakes
+	opponents := state.Board.Snakes
+
+	for _, opponent := range opponents {
+		for _, bodyPart := range opponent.Body {
+			// head is below a body part, don't move up
+			if bodyPart.X == myHead.X && bodyPart.Y == myHead.Y+1 {
+				isMoveSafe["up"] = false
+			}
+			// head is above a body part, don't move down
+			if bodyPart.X == myHead.X && bodyPart.Y == myHead.Y-1 {
+				isMoveSafe["down"] = false
+			}
+
+			// head is left of a body part, don't move left
+			if bodyPart.X == myHead.X-1 && bodyPart.Y == myHead.Y {
+				isMoveSafe["left"] = false
+			}
+
+			// head is right of a body part, don't move right
+			if bodyPart.X == myHead.X+1 && bodyPart.Y == myHead.Y {
+				isMoveSafe["right"] = false
+			}
+		}
+	}
 
 	// Are there any safe moves left?
 	safeMoves := []string{}
